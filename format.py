bf8be931350c9208b96b335d1f3d8b63361a5953
thalestmm <meier.thales@gmail.com>
2025-03-21 23:11:28 -0300

    Dictionary file pre-processing

---
+++ format.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+def main():
+    we = WordsEditor("words.txt")
+    we.execute()
+
+
+class WordsEditor:
+    def __init__(self, target_file: str) -> None:
+        self.filepath = target_file
+        self.df = pd.read_table(self.filepath)
+
+    def letter_counter(self):
+        self.df["letters"] = self.df["word"].str.len()
+
+    def identify_symbols(self):
+        symbols = "Á|Â|Ã|À|É|Ê|Í|Ó|Õ|Ô|Ç"
+        self.df["has_symbol"] = (self.df["word"].str.contains(symbols))
+        pass
+
+    def upper_everything(self):
+        self.df["word"] = self.df["word"].str.upper()
+        pass
+
+    def export(self):
+        self.df.to_csv("game_ready.csv", index=False)
+
+    def execute(self):
+        self.letter_counter()
+        self.upper_everything()
+        self.identify_symbols()
+        self.export()
+
+
+if __name__ == "__main__":
+    main()
