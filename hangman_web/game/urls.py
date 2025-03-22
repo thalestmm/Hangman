4e7fd53b168d6f08ee7a98bce9cc2267c201bc00
thalestmm <meier.thales@gmail.com>
2025-03-22 11:06:45 -0300

    basic ui added, still lacking overall functionality

--- hangman_web/game/urls.py
+++ hangman_web/game/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('deduct', views.deduct, name='deduct'),
+    path('play', views.play, name='play'),
+    path('update', views.update, name='update'),
 ]
--- hangman_web/game/views.py
+++ hangman_web/game/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
+    # TODO: Add hard mode (with symbols)
     return render(request, 'game/index.html')
 
 def deduct(request):
@@ -11,3 +12,29 @@ def deduct(request):
         "life": 7
     }
     return render(request, 'game/life.html', context)
+
+def play(request):
+    body = request.POST
+    word_size = int(int(body['word-size'])/100*21 + 2)
+    print(word_size)
+    word = ""
+    for i in range(word_size):
+        word += "_ "
+    word.rstrip(" ")
+    # TODO: Select random word
+    context = {
+        "lifes": 10,
+        "word": word,
+    }
+    return render(request, 'game/play.html', context)
+
+def update(request):
+    # TODO: Return invalid form if input is not a single letter
+    # TODO: Change input to upper case
+    # TODO: If life reaches 0 and word is not complete, return GAME OVER
+    print(request.POST)
+    body = request.POST
+    context = {
+        "lifes": int(body['remaining-lifes'])-1,
+    }
+    return render(request, 'game/play.html', context)
